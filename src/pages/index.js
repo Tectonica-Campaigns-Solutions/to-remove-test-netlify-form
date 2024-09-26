3948b4f09118d5f618ed92fe6832b6eca083fb6b
marcelsafont <marcel.safont1983@gmail.com>
2024-09-26 17:40:50 +0200

    rollback to previous form

--- src/pages/index.js
+++ src/pages/index.js
@@ -126,20 +126,21 @@ const links = [
 const IndexPage = () => {
   return (
     <div>
-     <form name="contact" netlify>
-        <p>
-          <label>Name <input type="text" name="name" /></label>
-        </p>
-        <p>
-          <label>Email <input type="email" name="email" /></label>
-        </p>
-        <p>
-          <label>Consents <input type="checkbox" name="consent" /></label>
-        </p>
-        <p>
-          <button type="submit">Send</button>
-        </p>
-      </form>
+      <form name="Contact Form" method="POST" data-netlify="true">
+      <input type="hidden" name="form-name" value="Contact Form" />
+      <div>
+        <label>Your Email:</label>
+        <input type="email" name="email" />
+      </div>
+      <div>
+        <label>Message:</label>
+        <textarea name="message" />
+        </div>
+        <div>
+          <input type="checkbox" name="consent" value="yes"/>
+        </div>
+      <button type="submit">Send</button>
+    </form>
     </div>
   )
 }
